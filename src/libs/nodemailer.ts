62b9ff684344c763432a676f2c24c47ece8ebdbc
Hepytech Dev <hepytech@hallomepy.com>
2026-02-07 07:48:02 +0700

    fix: fix mailer issue

--- src/libs/nodemailer.ts
+++ src/libs/nodemailer.ts
@@ -1,9 +1,11 @@
 import nodemailer from "nodemailer";
 
 export const transportEmail = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
-    }
-})
+  host: process.env.SMTP_HOST,
+  port: Number(process.env.SMTP_PORT),
+  secure: true,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
